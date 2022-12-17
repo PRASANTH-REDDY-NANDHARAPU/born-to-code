a00ea32a7aec52d625a6edfa876513d745e28993
PRASANTH REDDY NANDHARAPU <109941441+PRASANTH-REDDY-NANDHARAPU@users.noreply.github.com>
2022-12-17 12:27:46 +0530

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,174 @@
+function login() {
+    let username = document.getElementById("videos-username").value;
+    let password = document.getElementById("videos-password").value;
+    if (username == "prasanth" && password == "prakarthik") {
+        open("videos.html");
+    } else if (username == "padmavathi" && password == "daddy@123") {
+        open("videos.html");
+    } else if (username == "himavarsha" && password == "lokeshreddy") {
+        open("videos.html");
+    } else if (username == "reddyhareesh" && password == "hareesh#2003") {
+        open("videos.html");
+    }else if (username == "nikhitha3112" && password == "nikki@3106") {
+        open("videos.html");
+    }else if (username == "munibabu0602" && password == "muni9100") {
+        open("videos.html");
+    }else if (username == "sumiyavalluru" && password == "mv@hdsr04") {
+        open("videos.html");
+    }else if (username == "venkatacharan" && password == "charan@123") {
+        open("videos.html");
+    }else if (username == "tejaswar" && password == "teja12345") {
+        open("videos.html");
+    }else if (username == "sameerbasha" && password == "sameer@786") {
+        open("videos.html");
+    }else if (username == "ramudu@955" && password == "ram@123") {
+        open("videos.html");
+    }else if (username == "ruchithareddy" && password == "ruchi99632") {
+        open("videos.html");
+    }else if (username == "maramkusuma" && password == "kusu@123") {
+        open("videos.html");
+    }else if (username == "mounikamodem" && password == "mounika@123") {
+        open("videos.html");
+    } else if (username == "ysusritha" && password == "susritha12") {
+        open("videos.html");
+    }else if (username == "gsahithi" && password == "sahithi19") {
+        open("videos.html");
+    }else if (username == "mdivyajyothi" && password == "divya5248") {
+        open("videos.html");
+    }else if (username == "yogeeswar" && password == "630yogeeswar") {
+        open("videos.html");
+    }else if (username == "divya" && password == "divya@207") {
+        open("videos.html");
+    }else if (username == "pigilambhargav" && password == "bhargav@3039") {
+        open("videos.html");
+    }else if (username == "vineethasriram" && password == "hanuman123") {
+        open("videos.html");
+    }else if (username == "sahajtejm" && password == "sahajram2004") {
+        open("videos.html");
+    }else if (username == "maheshbabu1204" && password == "mahesh@1204") {
+        open("videos.html");
+    }else if (username == "sandeep" && password == "sandeepdhee") {
+        open("videos.html");
+    }else if (username == "madhuri@483" && password == "204m1a0483") {
+        open("videos.html");
+    }else if (username == "npadmini" && password == "padmini88979") {
+        open("videos.html");
+    }else if (username == "dhanushmanikanta" && password == "dhanush2003") {
+        open("videos.html");
+    }else if (username == "rajulove" && password == "rajulove143") {
+        open("videos.html");
+    }else if (username == "lavanyadussa" && password == "lavanyadussa10") {
+        open("videos.html");
+    }else if (username == "sravya" && password == "sravya90145") {
+        open("videos.html");
+    }else if (username == "bsaipoojitha" && password == "lavanya") {
+        open("videos.html");
+    }else if (username == "rajagaripradeep" && password == "ab@17") {
+        open("videos.html");
+    }else if (username == "nthyagarajulu" && password == "thyagaraj485") {
+        open("videos.html");
+    }else if (username == "sujith#@83" && password == "sujith@15") {
+        open("videos.html");
+    }else if (username == "pooja" && password == "pooja4023") {
+        open("videos.html");
+    }else if (username == "sneha" && password == "sneha@123") {
+        open("videos.html");
+    }else if (username == "madhankumar" && password == "m@dh@n13") {
+        open("videos.html");
+    }else if (username == "chandanak" && password == "8374934709") {
+        open("videos.html");
+    }else if (username == "gayathrik" && password == "brosis123") {
+        open("videos.html");
+    }else if (username == "pavan" && password == "7093158530p") {
+        open("videos.html");
+    }else if (username == "rakeshreddy" && password == "rake49477") {
+        open("videos.html");
+    }else if (username == "ranjithreddy" && password == "ranjith5507") {
+        open("videos.html");
+    }else if (username == "karthik183" && password == "karthik5428") {
+        open("videos.html");
+    }else if (username == "meghanavakati" && password == "saritha@09") {
+        open("videos.html");
+    }else if (username == "kiran" && password == "kiran0307") {
+        open("videos.html");
+    }else if (username == "jyothisankar" && password == "sankar418818") {
+        open("videos.html");
+    }else if (username == "kavyamomidi" && password == "kavya2003") {
+        open("videos.html");
+    }else if (username == "ashika" && password == "0508") {
+        open("videos.html");
+    }else if (username == "nvjaswanth" && password == "jaswanthnv128") {
+        open("videos.html");
+    }else if (username == "pujitha" && password == "pujitha@2004") {
+        open("videos.html");
+    }else if (username == "chandananeeli" && password == "chandana58") {
+        open("videos.html");
+    }else if (username == "afrin" && password == "afrin@7013") {
+        open("videos.html");
+    }else if (username == "pharshitha" && password == "harshitha13") {
+        open("videos.html");
+    }else if (username == "shaikrafia" && password == "rafia8385") {
+        open("videos.html");
+    }else if (username == "shaiksajida" && password == "sajida1712") {
+        open("videos.html");
+    }else if (username == "namurugopika" && password == "gopika16") {
+        open("videos.html");
+    }else if (username == "saimukesh" && password == "msai123@") {
+        open("videos.html");
+    }else if (username == "eswarkumar" && password == "eswarkumar@9391010") {
+        open("videos.html");
+    }else if (username == "kadirividyabhavani" && password == "vidya575") {
+        open("videos.html");
+    }else if (username == "sowjanyagoud" && password == "sowji@3124") {
+        open("videos.html");
+    }else if (username == "mjithendra" && password == "jithendra123") {
+        open("videos.html");
+    }else if (username == "mrishitha" && password == "rishitha@17") {
+        open("videos.html");
+    }else if (username == "fareedsk" && password == "vfvf2810") {
+        open("videos.html");
+    }else if (username == "vaishnavigayathriy" && password == "yvgnanu37") {
+        open("videos.html");
+    }else if (username == "tharunkumar" && password == "tharun2860") {
+        open("videos.html");
+    }else if (username == "syedhazeera" && password == "786hazeera") {
+        open("videos.html");
+    }else if (username == "shaikruffiya" && password == "ruffiya1432") {
+        open("videos.html");
+    }else if (username == "kiran" && password == "kiran00373") {
+        open("videos.html");
+    }else if (username == "jyothisankar" && password == "sankar418818") {
+        open("videos.html");
+    }else if (username == "yasmeen" && password == "yas@245") {
+        open("videos.html");
+    }else if (username == "yakshitha" && password == "akshi02") {
+        open("videos.html");
+    }else if (username == "ymadhuri" && password == "madhuri14") {
+        open("videos.html");
+    }else if (username == "lasyareddy" && password == "lasya22") {
+        open("videos.html");
+    }else {
+        alert("YOU ARE NOT A MEMBER INSIDE BTC 😢");
+    }
+}
+
+
+
+// ----------function for the contact us page--------------
+function thanks(){
+    alert('🎁 HEYY YOUR RESPONSE HAS BEEN RECORDED SUCCESSFULLY')
+    open('index.html')
+}
+// ----------function for the home page--------------
+function home(){
+    alert('😅 RIGHT NOW YOU ARE STAYING IN (HOME) PAGE ONLY')
+}
+// ----------function for the ABOUT page--------------
+function about(){
+    alert('😅 RIGHT NOW YOU ARE STAYING IN (ABOUT) PAGE ONLY')
+}
+// ----------function for the ABOUT page--------------
+function contact(){
+    alert('😅 RIGHT NOW YOU ARE STAYING IN (CONTACT) PAGE ONLY')
+}
+

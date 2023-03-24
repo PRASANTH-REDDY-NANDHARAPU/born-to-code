a779f2faadd7f0c9aca9f190b32a4aac4ca597f2
PRASANTH REDDY NANDHARAPU <109941441+PRASANTH-REDDY-NANDHARAPU@users.noreply.github.com>
2023-03-24 18:35:13 +0530

    Add files via upload

---
+++ videos2.js
@@ -0,0 +1,172 @@
+function login() {
+  let username = document.getElementById("videos-username").value;
+  let password = document.getElementById("videos-password").value;
+  if (username == "prasanth" && password == "prakarthik") {
+    open("videos2.html");
+  } else if (username == "bindu" && password == "bindu222") {
+    open("videos2.html");
+  } else if (username == "swathi" && password == "swathi123") {
+    open("videos2.html");
+  } else if (username == "sushma" && password == "sushma9988") {
+    open("videos2.html");
+  } else if (username == "pallavi" && password == "pallavi9858") {
+    open("videos2.html");
+  } else if (username == "sameena" && password == "sameena123") {
+    open("videos2.html");
+  } else if (username == "sushmasreee" && password == "sushma123") {
+    open("videos2.html");
+  } else if (username == "balasubramanyam" && password == "balu987") {
+    open("videos2.html");
+  } else if (username == "priyareddy" && password == "priya123") {
+    open("videos2.html");
+  } else if (username == "Bhargavi" && password == "Bhargavi234") {
+    open("videos2.html");
+  } else if (username == "sai sree" && password == "sree123") {
+    open("videos2.html");
+  } else if (username == "sathya sai keerthana" && password == "keerthana345") {
+    open("videos2.html");
+  } else if (username == "saranya" && password == "saranya123") {
+    open("videos2.html");
+  } else if (username == "sai niharika" && password == "harika654") {
+    open("videos2.html");
+  } else if (username == "sai krishna" && password == "krishna555") {
+    open("videos2.html");
+  } else if (username == "sravani" && password == "sravani987") {
+    open("videos2.html");
+  } else if (username == "bhargavreddy" && password == "bhargav123") {
+    open("videos2.html");
+  } else if (username == "gayathri" && password == "gayathri333") {
+    open("videos2.html");
+  } else if (username == "rupasri" && password == "rupa987") {
+    open("videos2.html");
+  } else if (username == "hemalatha" && password == "hemalatha555") {
+    open("videos2.html");
+  } else if (username == "harika" && password == "harika65") {
+    open("videos2.html");
+  } else if (username == "gnana prasuna" && password == "prasuna111") {
+    open("videos2.html");
+  } else if (username == "manasa" && password == "manasa7876") {
+    open("videos2.html");
+  } else if (username == "swathi" && password == "swathi222") {
+    open("videos2.html");
+  } else if (username == "durga prasad" && password == "durga345") {
+    open("videos2.html");
+  } else if (username == "vanicute" && password == "vani111") {
+    open("videos2.html");
+  } else if (username == "mydhili" && password == "mydhili132") {
+    open("videos2.html");
+  } else if (username == "harika" && password == "harika55") {
+    open("videos2.html");
+  } else if (username == "reshma" && password == "reshma999") {
+    open("videos2.html");
+  } else if (username == "mahidhar" && password == "mahidhar234") {
+    open("videos2.html");
+  } else if (username == "divya" && password == "divya345") {
+    open("videos2.html");
+  } else if (username == "meghana" && password == "meghana608") {
+    open("videos2.html");
+  } else if (username == "reshma" && password == "reshu111") {
+    open("videos2.html");
+  } else if (username == "gayathri" && password == "gayathri9988") {
+    open("videos2.html");
+  } else if (username == "seshasai" && password == "sai765") {
+    open("videos2.html");
+  } else if (username == "uday kiran" && password == "uday9988") {
+    open("videos2.html");
+  } else if (username == "srigayathri" && password == "gayathri549") {
+    open("videos2.html");
+  } else if (username == "venkatesh" && password == "venkatesh 222") {
+    open("videos2.html");
+  } else if (username == "mamatha" && password == "mamatha414") {
+    open("videos2.html");
+  } else if (username == "divya sree" && password == "divya65") {
+    open("videos2.html");
+  } else if (username == "vaarshika" && password == "vaarshi55") {
+    open("videos2.html");
+  } else if (username == "krishna vamsi" && password == "vamsi 222") {
+    open("videos2.html");
+  } else if (username == "venkat rohith" && password == "rohith555") {
+    open("videos2.html");
+  } else if (username == "lekhana" && password == "lekhana234") {
+    open("videos2.html");
+  } else if (username == "sandhya" && password == "sandhya987") {
+    open("videos2.html");
+  } else if (username == "aishwarya" && password == "aishwarya123") {
+    open("videos2.html");
+  } else if (username == "sunaina" && password == "sunaina234") {
+    open("videos2.html");
+  } else if (username == "sushma sree" && password == "sushma987") {
+    open("videos2.html");
+  } else if (username == "Bhargavi" && password == "Bhargavi234") {
+    open("videos2.html");
+  } else if (username == "gbhavana" && password == "gbhavana@npm") {
+    open("videos2.html");
+  } else if (username == "babureddy" && password == "reddy@sdr") {
+    open("videos2.html");
+  } else if (username == "reddy" && password == "reddy@mgr") {
+    open("videos2.html");
+  } else if (username == "tanmaieesai" && password == "tanmaiee000") {
+    open("videos2.html");
+  } else if (username == "sruthija" && password == "sruthija987") {
+    open("videos2.html");
+  } else if (username == "suneetha" && password == "suneetha876") {
+    open("videos2.html");
+  } else if (username == "lakshman" && password == "lakshman988") {
+    open("videos2.html");
+  } else if (username == "akshayapriya" && password == "akshaya765") {
+    open("videos2.html");
+  } else if (username == "nirvighna" && password == "nirvighna65") {
+    open("videos2.html");
+  } else if (username == "nehayadav" && password == "neha665") {
+    open("videos2.html");
+  } else if (username == "sravika" && password == "sravika123") {
+    open("videos2.html");
+  } else if (username == "sowmya" && password == "sowmya111") {
+    open("videos2.html");
+  } else if (username == "suchini" && password == "suchini 345") {
+    open("videos2.html");
+  } else if (username == "divyasri" && password == "divya234") {
+    open("videos2.html");
+  } else if (username == "rajasekhar" && password == "rajasekhar123") {
+    open("videos2.html");
+  } else if (username == "Busi Nagendramma" && password == "Nagendramma111") {
+    open("videos2.html");
+  } else if (username == "lasya" && password == "lasya299") {
+    open("videos2.html");
+  } else if (username == "sai likhith" && password == "likhith333") {
+    open("videos2.html");
+  } else if (username == "poojitha" && password == "poojitha23") {
+    open("videos2.html");
+  } else if (username == "karthik reddy" && password == "karthik974") {
+    open("videos2.html");
+  } else if (
+    username == "Dwarasala Mallikarjuna" &&
+    password == "mallikarjuna234"
+  ) {
+    open("videos2.html");
+  } else if (username == "hemanth kumar" && password == "hemu332") {
+    open("videos2.html");
+  } else if (username == "umar abid" && password == "umar03") {
+    open("videos2.html");
+  } else if (username == "keerthana" && password == "keerthana2003") {
+    open("videos2.html");
+  } else if (username == "vamsi krishna" && password == "vamsi1756") {
+    open("videos2.html");
+  } else if (username == "bhargav sai" && password == "sai777") {
+    open("videos2.html");
+  } else if (username == "vamsikrishna" && password == "vamsi777") {
+    open("videos2.html");
+  } else if (username == "vijay" && password == "vijay549") {
+    open("videos2.html");
+  } else if (username == "nikitha" && password == "nikitha555") {
+    open("videos2.html");
+  } else if (username == "ajay kumar" && password == "ajay444") {
+    open("videos2.html");
+  } else if (username == "sumayaa" && password == "sumayaa987") {
+    open("videos2.html");
+  } else if (username == "kjyothi" && password == "kjyothi@qlr") {
+    open("videos2.html");
+  } else {
+    alert("YOU ARE NOT A MEMBER INSIDE BTC 😢");
+  }
+}
